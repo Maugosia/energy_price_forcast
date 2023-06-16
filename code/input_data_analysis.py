05e26137d2b0fee13b23e9ea2a47d9a6053b2e75
Michał Szustowicz <miszust@gmail.com>
2023-06-16 20:04:08 +0200

    2

--- code/input_data_analysis.py
+++ code/input_data_analysis.py
@@ -42,19 +42,22 @@ def plot_histogram(myData, binsCount, yScale):
 
 if __name__ == "__main__":
 
-    path_data = "../real_time_data/TH_NP15_data.csv"
+    path_data = "../day_ahead_data/PGAE_data.csv"
     data = pd.read_csv(path_data, sep=" |,", engine='python', names=["date", "time", "am-pm", "price", "zone"], skiprows=[0]).head(54912)
-    
+    print(path_data)
     print("Min value", min(data["price"]))
     print("Max value", max(data["price"]))
     x = np.quantile(data["price"], [0.25, 0.5, 0.75])
-    rozstęp = x[2]-x[0];
-    wąsD = x[0]-1.5*rozstęp;
-    wąsG = x[2]+1.5*rozstęp;
+    rozstęp = x[2]-x[0]
+    wąsD = x[0]-1.5*rozstęp
+    wąsG = x[2]+1.5*rozstęp
     print("Wąs dolny", wąsD)
     print("Wąs górny", wąsG)
+    print("Mediana ", x[1])
+    print("Średnia ", np.average(data["price"]))
+    print("Odchylenie standartowe ", np.std(data["price"]))
     count = sum(map(lambda x1: x1 > wąsG, data["price"]))
     print(count, "wartości odstających z", len(data["price"]), "(", int(count * 100 / len(data["price"])), "%)")
     # day ahead =720; realtime =8640
-    plot_temporal_relation(data, 8640, "linear")
-    plot_histogram(data, 20, "log")
+    #plot_temporal_relation(data, 8640, "linear")
+    #plot_histogram(data, 20, "log")
